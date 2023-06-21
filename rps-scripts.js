363514353fc76a2f03153a43823d92be2e8a9cff
TheresaH <44990136+theresah95@users.noreply.github.com>
2023-06-21 10:49:15 -0700

    files added and getComputerChoice function completed

---
+++ rps-scripts.js
@@ -0,0 +1,20 @@
+function getComputerChoice () {
+
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
+
+    let num = (Math.floor(Math.random()* 3));
+    
+    if(num == 0){
+        return rock;
+    } else if (num == 1){
+        return paper;
+    } else if (num == 2){
+        return scissors;
+    };
+};
+
+//To test getComputerChoice
+// console.log(getComputerChoice());
+
