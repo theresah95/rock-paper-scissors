a4ea4c3261c7211c75e40f8f33f52353e073d9a5
TheresaH <44990136+theresah95@users.noreply.github.com>
2023-06-21 11:58:13 -0700

    added game function

--- rps-scripts.js
+++ rps-scripts.js
@@ -34,20 +34,60 @@ function playRound(playerSelection, computerSelection) {
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
 
-    let playerWins = "You win!";
-    let computerWins = "Computer wins!";
-
     if (player == "rock" && computer == "scissors") {
-        return `${playerWins} Rock Beats Scissors.`;
+        return 1;
     } else if (player == "paper" && computer == "rock") {
-        return `${playerWins} Paper Beats Rock.`;
+        return 1;
     } else if (player == "scissors" && computer == "paper") {
-        return `${playerWins} Scissors Beats Paper.`;
+        return 1;
+    } else if (player == computer) {
+        return "tie";
     } else {
-        return `${computerWins} Rock Beats Paper.`;
+        return 0;
     };
 
 };
 
-console.log(playRound("rock", getComputerChoice()));
+//To test playRound function
+// console.log(playRound("rock", getComputerChoice()));
 
+/* game() function
+ * playRound() is used inside this function
+ * Get user input for the player's move
+ * Play five rounds and keep track of who wins
+ * Display the winner at the end of five rounds
+ */
+function game() {
+
+    let playerTotal = 0;
+    let computerTotal = 0;
+
+    for (let i=0; i<5; i++){
+        //Get Player's Selection
+        let playerSelection = prompt("Rock, paper, or scissors?");
+        console.log("You played: " + playerSelection);
+        //Get Computer's Selection
+        let computerSelection = getComputerChoice();
+        console.log("Computer played: " + computerSelection); 
+        //Play a round and store result
+        let result = playRound(playerSelection, computerSelection);
+
+        //Store results in tally
+        if (result == 1){
+            playerTotal = playerTotal + 1;
+        } else {
+            computerTotal = computerTotal + 1;
+        }  
+    }
+
+    //Check totals
+    if (playerTotal > computerTotal) {
+        //Player wins
+        console.log(`You win! ${playerTotal} vs. ${computerTotal}`);
+    } else {
+        //Computer wins
+        console.log(`Computer wins. ${computerTotal} vs. ${playerTotal}`)
+    }
+};
+
+game();
